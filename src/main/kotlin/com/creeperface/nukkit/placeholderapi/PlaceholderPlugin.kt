3ce490552e411d09fa95dfdc96c2ea6b60891aac
Creeperface01 <honzaxp01@gmail.com>
2021-03-19 13:13:17 +0100

    Test

--- src/main/kotlin/com/creeperface/nukkit/placeholderapi/PlaceholderPlugin.kt
+++ src/main/kotlin/com/creeperface/nukkit/placeholderapi/PlaceholderPlugin.kt
@@ -1,8 +1,6 @@
 package com.creeperface.nukkit.placeholderapi
 
 import cn.nukkit.plugin.PluginBase
-import cn.nukkit.utils.PluginException
-import com.creeperface.nukkit.placeholderapi.util.KotlinLibDownloader
 
 /**
  * @author CreeperFace
@@ -12,10 +10,6 @@ class PlaceholderPlugin : PluginBase() {
     private lateinit var api: PlaceholderAPIIml
 
     override fun onLoad() {
-        if (!KotlinLibDownloader.check(this)) {
-            throw PluginException("KotlinLib could not be found")
-        }
-
         api = PlaceholderAPIIml.createInstance(this)
     }
 
